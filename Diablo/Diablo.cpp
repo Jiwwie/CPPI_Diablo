0f1ea1b0baba469d9da1f2b9a893444a4d83bbed
Jiwwi <lazyjiwwi@gmail.com>
2026-09-23 22:03:41 +0200

    Update Diablo.cpp

--- Diablo/Diablo.cpp
+++ Diablo/Diablo.cpp
@@ -2,9 +2,13 @@
 #include <vector>
 #include "Player.h"
 #include "Enemy.h"
+#include "Room.h"
 
 int main()
 {
+    int currentRoom = 0;
+    std::vector<Room> rooms;
+
     Player player;
     Enemy enemy(50, 5);
 
