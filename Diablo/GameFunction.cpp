393e4311ae3a2c09f08eeb168db6c23478d73add
Jiwwi <lazyjiwwi@gmail.com>
2026-09-24 14:24:51 +0200

    move battle()

--- Diablo/GameFunction.cpp
+++ Diablo/GameFunction.cpp
@@ -1,2 +1,23 @@
 #include "GameFunction.h"
+#include "Room.h"
+#include "Player.h"
 
+void GameFunction::Battle(Player& aPlayer, std::vector<Room> someRooms)
+{
+    while (someRooms[aPlayer.GetCurrentRoom()].myEnemies.size() > 0 && aPlayer.isAlive())
+    {
+        int chosenEnemy = 0;
+        std::cout << "Enemies in room: " << someRooms[aPlayer.GetCurrentRoom()].myEnemies.size();
+
+        aPlayer.ShowStats();
+        someRooms[aPlayer.GetCurrentRoom()].DisplayEnemies();
+
+        std::cout << "Chose enemy to attack:\n";
+        std::cin >> chosenEnemy;
+        system("pause");
+
+        aPlayer.DoDamage(someRooms[aPlayer.GetCurrentRoom()].myEnemies[chosenEnemy]);
+        someRooms[aPlayer.GetCurrentRoom()].KillEnemy(chosenEnemy);
+    }
+
+}
--- Diablo/GameFunction.h
+++ Diablo/GameFunction.h
@@ -4,25 +4,7 @@
 
 namespace GameFunction
 {
-	inline void Battle(Player& aPlayer, std::vector<Room> someRooms)
-	{
-        while (someRooms[aPlayer.GetCurrentRoom()].myEnemies.size() > 0 && aPlayer.isAlive())
-        {
-            int chosenEnemy = 0;
-            std::cout << "Enemies in room: " << someRooms[aPlayer.GetCurrentRoom()].myEnemies.size();
-
-            aPlayer.ShowStats();
-            someRooms[aPlayer.GetCurrentRoom()].DisplayEnemies();
-
-            std::cout << "Chose enemy to attack:\n";
-            std::cin >> chosenEnemy;
-            system("pause");
-
-            aPlayer.DoDamage(someRooms[aPlayer.GetCurrentRoom()].myEnemies[chosenEnemy]);
-            someRooms[aPlayer.GetCurrentRoom()].KillEnemy(chosenEnemy);
-        }
-
-	}
+    void Battle(Player& aPlayer, std::vector<Room> someRooms);
 
 
 }
